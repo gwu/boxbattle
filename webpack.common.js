18d1cb78d0ce9e10de250ede2d7620142c11b28a
Garrett Wu <wugarrett@gmail.com>
2017-09-20 11:19:28 -0400

    Add linting, prod/dev builds

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['standard-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'index.ejs'
+    })
+  ]
+}
