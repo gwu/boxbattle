223ac0dc76de31b753b6ae7c2b3be5b1b36ddc39
Garrett Wu <wugarrett@gmail.com>
2018-01-23 14:04:44 +0700

    Cursor and movement

--- src/game.js
+++ src/game.js
@@ -24,9 +24,88 @@ export class PlayingField {
         this._view.addChild(block)
       })
     })
+
+    this._cursor = new Cursor({
+      size: this._size,
+      width: this._width,
+      height: this._height
+    })
+    this._view.addChild(this._cursor.view)
   }
 
   get view () {
     return this._view
+  }
+
+  get cursor () {
+    return this._cursor
+  }
+}
+
+class Cursor {
+  constructor (opts) {
+    this._x = 0
+    this._y = 0
+    this._size = opts.size
+    this._width = opts.width
+    this._height = opts.height
+    this._view = new PIXI.Container()
+    this.x = 0
+    this.y = 0
+
+    const frame = new PIXI.Graphics()
+    frame.lineStyle(4, 0xffffff, 1)
+    frame.moveTo(0, 0)
+    frame.lineTo(this._size * 2, 0)
+    frame.lineTo(this._size * 2, this._size)
+    frame.lineTo(0, this._size)
+    frame.lineTo(0, 0)
+    this._view.addChild(frame)
+
+    window.addEventListener('keydown', (event) => {
+      switch (event.key) {
+        case 'ArrowUp':
+          this.y++
+          break
+        case 'ArrowDown':
+          this.y--
+          break
+        case 'ArrowLeft':
+          this.x--
+          break
+        case 'ArrowRight':
+          this.x++
+          break
+      }
+      event.preventDefault()
+    })
+  }
+
+  get view () {
+    return this._view
+  }
+
+  get x () {
+    return this._x
+  }
+
+  set x (val) {
+    if (val < 0 || val >= this._width - 1) {
+      return
+    }
+    this._x = val
+    this._view.x = val * this._size
+  }
+
+  get y () {
+    return this._y
+  }
+
+  set y (val) {
+    if (val < 0 || val >= this._height) {
+      return
+    }
+    this._y = val
+    this._view.y = (this._height - val - 1) * this._size
   }
 }
